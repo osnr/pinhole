07f8e1fc00d5dcd2da58a71916902c721bee1404
Omar Rizwan <omar.rizwan@gmail.com>
2014-08-03 01:39:09 -0700

    add load/save for levels

--- Pinhole/Level/Level.hs
+++ Pinhole/Level/Level.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Level.Level
 ( Level (Level)
 , spawn
@@ -11,6 +13,8 @@ module Level.Level
 where
 
 import Graphics.Gloss.Data.Point
+import Data.Aeson
+import GHC.Generics
 
 screenWidth :: Num a => a
 screenWidth = 640
@@ -23,23 +27,30 @@ data Level = Level {
     , goal :: Goal
 
     , walls :: [Wall]
-} deriving (Show)
+} deriving (Show, Generic)
+
+instance FromJSON Level
+instance ToJSON Level
 
 type Goal = (Point, Point)
 
 data Wall = Wall Point Point
-            deriving (Show, Eq)
+            deriving (Show, Eq, Generic)
+
+instance FromJSON Wall
+instance ToJSON Wall
 
 initialLevel :: Level
-initialLevel = Level { spawn = (0, 240)
-                     , goal = ((0, -240), (20, 0))
+initialLevel =
+    Level { spawn = (0, 240)
+          , goal = ((0, -240), (20, 0))
 
-                     , walls = [ Wall (-screenWidth/2, -screenHeight/2 + 1)
-                                          (-screenWidth/3, -screenHeight/2 + 1) -- bottom-left
-                               , Wall (-screenWidth/4, -screenHeight/2 + 1)
-                                          (screenWidth/2, -screenHeight/2 + 1) -- bottom-right
-                               , Wall (-screenWidth/2 + 1, -screenHeight/2)
-                                          (-screenWidth/2 + 1, screenHeight/2) -- left
-                               , Wall (screenWidth/2 - 1, -screenHeight/2)
-                                          (screenWidth/2 - 1, screenHeight/2) -- right
-                               ] }
+          , walls = [ Wall (-screenWidth/2, -screenHeight/2 + 1)
+                               (-screenWidth/3, -screenHeight/2 + 1) -- bottom-left
+                    , Wall (-screenWidth/4, -screenHeight/2 + 1)
+                               (screenWidth/2, -screenHeight/2 + 1) -- bottom-right
+                    , Wall (-screenWidth/2 + 1, -screenHeight/2)
+                               (-screenWidth/2 + 1, screenHeight/2) -- left
+                    , Wall (screenWidth/2 - 1, -screenHeight/2)
+                               (screenWidth/2 - 1, screenHeight/2) -- right
+                    ] }
--- Pinhole/Main.hs
+++ Pinhole/Main.hs
@@ -1,4 +1,11 @@
-import Graphics.Gloss.Interface.Pure.Game
+import System.Environment
+import System.Directory
+import qualified Data.ByteString.Lazy as B
+import Control.Exception
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
+
+import Graphics.Gloss.Interface.IO.Game
 
 import Level.Level
 
@@ -13,12 +20,50 @@ screenWidth = 640
 screenHeight :: Num a => a
 screenHeight = 480
 
+loadLevel :: FilePath -> IO Level
+loadLevel path =
+    let loadLevel' = do
+          file <- B.readFile path
+          let argLevelM = decode file
+          case argLevelM of
+            Just lvl -> return lvl
+            Nothing  -> throwIO . userError $ "Invalid level file format"
+    in do lvlE <- try loadLevel'
+          case (lvlE :: Either SomeException Level) of
+            Left _    -> return initialLevel -- we don't print to stderr/stdout because it will crash Windows app
+            Right lvl -> return lvl
+
 main :: IO ()
-main =
-    play (InWindow "Pinhole" (screenWidth, screenHeight) (0, 0))
-         black
-         60
-         (playLevel initialLevel)
-         draw
-         handlePlayEvent
-         step
+main = do args <- getArgs
+          lvl <- case args of
+                   []       -> return initialLevel
+                   path : _ -> loadLevel path
+
+          playIO (InWindow "Pinhole" (screenWidth, screenHeight) (0, 0))
+                 black
+                 60
+                 (playLevel lvl)
+                 (return . draw)
+                 handleEvent
+                 (\dt pl -> return $ step dt pl)
+
+handleEvent :: Event -> PlayState -> IO PlayState
+handleEvent event pl =
+    do case event of
+         EventKey (Char 's') Up _ _ -> save pl 0
+         _ -> return ()
+       return $ handlePlayEvent event pl
+
+save :: PlayState -> Int -> IO ()
+save pl i = do let lvl = level pl
+                   lvl' = lvl { walls = drawnWalls pl ++ walls lvl }
+                   file = encodePretty lvl'
+
+                   suffix = show i
+                   suffix' = if length suffix < 2 then '0':suffix else suffix
+                   path = "level_" ++ suffix' ++ ".json"
+
+               alreadyExists <- doesFileExist path
+               if alreadyExists
+                 then save pl (i + 1)
+                 else B.writeFile path file
